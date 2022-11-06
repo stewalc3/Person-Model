44458ed099877a5ebe2ceb296707e7ee44036c91
stewalc3 <stewalc@mail.uc.edu>
2022-11-06 16:27:30 -0500

    Parts 1 and 2

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,26 +1,42 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import  java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class PersonGenerator {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         Scanner pipe = new Scanner(System.in);
-
+        String ID;
+        String fName;
+        String lName;
+        String title;
+        int YoB;
         int count = 0;
 
         while (true) {
             if (count == 0) {
-                String data = "";
-                data = SafeInput.getNonZeroLenString(pipe, "Enter data: ");
-                list.add(data);
+                ID = SafeInput.getNonZeroLenString(pipe, "Enter ID Number: ") + ",";
+                fName = SafeInput.getNonZeroLenString(pipe, "Enter First Name: ") + ",";
+                lName = SafeInput.getNonZeroLenString(pipe, "Enter Last Name: ") + ",";
+                title = SafeInput.getNonZeroLenString(pipe, "Enter Title: ") + ",";
+                YoB = SafeInput.getRangedInt(pipe, "Enter a birth year between, ", 1000, 9999);
+                list.add(ID + fName + lName + title + YoB);
                 count++;
             }//end if
             else if (count == 1) {
                 boolean YN = SafeInput.getYNConfirm(pipe, "Do you have more data to enter? ");
                 if (YN) {
-                    String data = "";
-                    data = SafeInput.getNonZeroLenString(pipe, "Enter data: ");
-                    list.add(data);
+                    ID = SafeInput.getNonZeroLenString(pipe, "Enter ID Number: ") + ",";
+                    fName = SafeInput.getNonZeroLenString(pipe, "Enter First Name: ") + ",";
+                    lName = SafeInput.getNonZeroLenString(pipe, "Enter Last Name: ") + ",";
+                    title = SafeInput.getNonZeroLenString(pipe, "Enter Title: ") + ",";
+                    YoB = SafeInput.getRangedInt(pipe, "Enter a birth year between, ", 1000, 9999);
+                    list.add(ID + fName + lName + title + YoB);
                 }//end if
                 else if (!YN) {
                     System.out.println("\n");
@@ -32,5 +48,33 @@ public class PersonGenerator {
             }//end while
 
         }//end outer while
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String rec : list)
+            {
+                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }//end main
 }
---
+++ src/PersonReader.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonReader {
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String data = "";
+
+        try
+        {
+
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                int line = 0;
+                System.out.println("ID#          Firstname     Lastname       Title    YOB");
+                System.out.println("=======================================================");
+                while(reader.ready())
+                {
+                    data = reader.readLine();
+                    line++;
+                    data = data.replaceAll(",", "        ");
+                    System.out.println(data);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
